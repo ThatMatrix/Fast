c0bbd859adab0fa84fa54a23e006103fd8a5fc8e
ThatMatrix <that_matrix12@protonmail.com>
2024-07-14 05:21:09 +0200

    feat(request): beginning parsing and can send 404 responses

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::Write,
+    io::{BufRead, Read, Write},
     net::{TcpListener, TcpStream},
+    path, str,
 };
 
 fn main() {
@@ -26,10 +27,33 @@ fn main() {
 }
 
 fn send_response(mut stream: TcpStream) {
-    let status_line = String::from("HTTP/1.1 200 OK\r\n\r\n");
+    println!("Parsing request");
+    let mut buffer: Vec<u8> = Vec::new();
+    // TODO if incomming connection does not close the stream, could hang forever
+    // TODO remove expect in code
+    stream
+        .read_to_end(&mut buffer)
+        .expect("Could not read request from stream");
+    let request = str::from_utf8(&buffer).expect("Could not parse request in utf8");
+    let mut iterator = request.split("\r\n");
+
+    let path_requested = iterator
+        .next()
+        .expect("Request must have a status line")
+        .split(" ")
+        .nth(1)
+        .expect("Status line must contain a path");
+    println!("Path requested = \"{}\"", path_requested);
+
+    println!("Sending response to stream");
+    let status_line = if path_requested.len() <= 1 {
+        String::from("HTTP/1.1 200 OK\r\n\r\n")
+    } else {
+        String::from("HTTP/1.1 404 Not Found\r\n\r\n")
+    };
+
     // TODO remove expect by handling error
     stream
         .write(status_line.as_ref())
         .expect("Couldn't write to stream");
-    println!("Sent response to stream")
 }
