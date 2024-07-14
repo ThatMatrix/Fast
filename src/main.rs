39fb1d1b6e0293572a44c541406dc9406ad0d394
ThatMatrix <that_matrix12@protonmail.com>
2024-07-14 05:01:10 +0200

    feat(basic_response): can now send 200 Ok responses to connections

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{
+    io::Write,
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    // println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
 
@@ -13,10 +16,20 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                send_response(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn send_response(mut stream: TcpStream) {
+    let status_line = String::from("HTTP/1.1 200 OK\r\n\r\n");
+    // TODO remove expect by handling error
+    stream
+        .write(status_line.as_ref())
+        .expect("Couldn't write to stream");
+    println!("Sent response to stream")
 }
