c1b1cf0ce9bf568c4d4697f49c7cb1dfd98dd545
ThatMatrix <that_matrix12@protonmail.com>
2024-07-16 03:00:36 +0200

    feat(refactor): made server into object + started result refacto

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,78 @@ use std::{
     str, thread,
 };
 
-fn main() {
-    let root_path = get_root_path();
-
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                // we have to clone here because we are inside a thread and modifying root_path might
-                // include modifying it for concurrent threads
-                let _root_path = root_path.clone();
-                println!("accepted new connection");
-                thread::spawn(|| send_response(_stream, _root_path));
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
-    }
+struct Server {
+    ip: String,
+    port: String,
+    root_directory: String,
+    thread_pool: Vec<std::thread::JoinHandle<Result<(), String>>>,
 }
 
-fn get_root_path() -> String {
+impl Server {
+    fn new(ip: String, port: String) -> Result<Server, String> {
+        Ok(Server {
+            ip,
+            port,
+            root_directory: match get_root_path() {
+                Ok(root_directory) => root_directory,
+                Err(err) => {
+                    return Err(format!(
+                        "Server::new() -> Could not infer root_directory: {}",
+                        err
+                    ))
+                }
+            },
+            thread_pool: vec![],
+        })
+    }
+
+    fn run(&mut self) -> Result<(), String> {
+        let bind_address = format!("{}:{}", &self.ip, &self.port);
+        let listener = match TcpListener::bind(bind_address.as_str()) {
+            Ok(tcp_listener) => tcp_listener,
+            Err(err) => {
+                return Err(format!(
+                    "Could not bind to target address {bind_address} - {err}",
+                ))
+            }
+        };
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(_stream) => {
+                    // we have to clone here because we are inside a thread and modifying root_path might
+                    // include modifying it for concurrent threads
+                    let _root_path = self.root_directory.clone();
+                    println!("accepted new connection");
+                    let handler = thread::spawn(|| send_response(_stream, _root_path));
+                    self.thread_pool.push(handler);
+                }
+                Err(e) => {
+                    println!("error: {}", e);
+                }
+            }
+        }
+        Ok(())
+    }
+
+    //TODO implem clean stop function to make sure all threads are closed
+}
+
+fn main() -> Result<(), String> {
+    let mut server = Server::new("127.0.0.1".to_string(), "4221".to_string())?;
+    server.run()
+}
+
+fn get_root_path() -> std::io::Result<String> {
     let mut args = std::env::args();
     while args.next().is_some_and(|arg| arg != "--directory") {
         continue;
     }
-    // TODO remove expect here and return nicely
     let root_path = args.next().unwrap_or_default();
-    root_path
+    Ok(root_path)
 }
 
-fn send_response(mut stream: TcpStream, mut root_path: String) {
+fn send_response(mut stream: TcpStream, mut root_path: String) -> Result<(), String> {
     println!("Parsing request");
     let mut buffer = [0; 2048];
 
@@ -45,7 +85,10 @@ fn send_response(mut stream: TcpStream, mut root_path: String) {
     let _nb_read = stream
         .read(&mut buffer)
         .expect("Could not read request from stream");
-    let request = str::from_utf8(&buffer).expect("Could not parse request in utf8");
+    let request = match str::from_utf8(&buffer) {
+        Ok(request) => request,
+        Err(err) => return Err(err.to_string()),
+    };
     let mut iterator = request.split("\r\n");
 
     // TODO remove expect
@@ -113,9 +156,11 @@ fn send_response(mut stream: TcpStream, mut root_path: String) {
     println!("Sending response to stream for {}", path_requested);
 
     // TODO remove expect by handling error
-    stream
-        .write(response.as_ref())
-        .expect("Couldn't write to stream");
+    match stream.write(response.as_ref()) {
+        Ok(nb_wrote) => nb_wrote,
+        Err(err) => return Err(format!("Could not write file - {err}")),
+    };
+    Ok(())
 }
 
 fn read_file(path: String) -> std::io::Result<String> {
