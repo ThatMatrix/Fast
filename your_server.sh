d699f38799018ee113287f1e5a2fe8dcd2f2b0c3
ThatMatrix <that_matrix12@protonmail.com>
2024-07-24 19:21:30 +0200

    fix(buildsystem): removed Codecrafters mentions

--- your_server.sh
+++ your_server.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
-#
-# DON'T EDIT THIS!
-#
-# CodeCrafters uses this file to test your code. Don't make any changes here!
-#
-# DON'T EDIT THIS!
 exec cargo run \
     --quiet \
     --release \
-    --target-dir=/tmp/codecrafters-http-server-target \
+    --target-dir=/tmp/fast\
     --manifest-path $(dirname $0)/Cargo.toml -- "$@"
